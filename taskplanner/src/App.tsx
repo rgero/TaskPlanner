426ab5ce72feb548715bb37d15d1644b6e624236
Roy Gero <roy.gero.ii@gmail.com>
2023-08-20 12:11:56 -0400

    Initial Commit to the project.

---
+++ taskplanner/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"logos/logo512.png"} className="App-logo" alt="logo" />
+        <p>
+          Just the generic home page that comes with creating a react project.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
