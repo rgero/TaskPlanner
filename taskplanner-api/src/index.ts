c6b03ff2e05e1d6114bfa1c97d0fe99860ef53f5
Roy Gero <roy.gero.ii@gmail.com>
2023-08-23 23:07:55 -0400

    Adding the Mongoose stater and appropriate dependencies.

--- taskplanner-api/src/index.ts
+++ taskplanner-api/src/index.ts
@@ -1,13 +1,22 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from "dotenv";
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
+import mongoose from './mongooseStarter';
+
+dotenv.config();
+const port = process.env.PORT;
+
+mongoose.start();
 const app:Express = express();
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req:Request, res:Response) => {
     res.send("HELLO ROYMOND");
 });
 
-dotenv.config();
-const port = process.env.PORT;
-
-app.listen(port);
+app.listen(port, ()=>{
+    console.log(`Server started, listening on port ${port}`);
+})
