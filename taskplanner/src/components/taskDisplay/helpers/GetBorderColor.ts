b165a65d9b11e470232d0b42ddc1c2f4b6653088
Roy Gero <roy.gero.ii@gmail.com>
2023-08-25 11:39:12 -0400

    I moved this function to the other file.

--- taskplanner/src/components/taskDisplay/helpers/GetBorderColor.ts
+++ taskplanner/src/components/taskDisplay/helpers/GetBorderColor.ts
@@ -12,15 +12,3 @@ export const getBorderColor = (status: TaskCounterStatusType): string => {
             return 'success.light';
     }
 }
-
-export const getCorrectTitle = (status: TaskCounterStatusType): string => {
-    switch(status)
-    {
-        case Status.todo:
-            return 'To Do'
-        case Status.inProgress:
-            return 'In Progress';
-        case Status.done:
-            return 'Completed';
-    }
-}
