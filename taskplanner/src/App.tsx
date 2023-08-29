c769ebbbd2da85ee2243dac7b68d5469d14da8a1
Roy Gero <roy.gero.ii@gmail.com>
2023-08-29 19:04:03 -0400

    Adding React-Query

--- taskplanner/src/App.tsx
+++ taskplanner/src/App.tsx
@@ -1,19 +1,26 @@
 import React, {FC, ReactElement} from 'react';
 import {ThemeProvider, CssBaseline} from "@mui/material"
 import { customTheme } from './theme/customTheme';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
 
 // My components
 import Dashboard from './pages/Dashboard';
 
+// Create a client for React Query
+const queryClient = new QueryClient();
 
 //Note: FC is Functional Component
 //      ReactElement is the JSX that's constucted for rendering.
 const App:FC = ():ReactElement  => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline/>
-      <Dashboard/>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline/>
+        <Dashboard/>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
   );
 }
 
