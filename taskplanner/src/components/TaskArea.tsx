d5677427c30af2bd29fa0fd16e02ebc880d0b882
Roy Gero <roy.gero.ii@gmail.com>
2023-08-30 19:59:02 -0400

    Starting to use the react-query to get data.

--- taskplanner/src/components/TaskArea.tsx
+++ taskplanner/src/components/TaskArea.tsx
@@ -1,11 +1,16 @@
 import React, {FC, ReactElement} from 'react';
 import { format } from 'date-fns';
-
+import { useQuery } from 'react-query';
 import { Box, Grid } from '@mui/material';
+
+import taskplannerAPI from '../api/taskplanner.api';
 import TaskStats from './taskDisplay/TaskStats';
 import TaskList from './taskDisplay/TaskList';
 
 const TaskArea:FC = ():ReactElement  => {
+
+    const {error, isLoading, data, refetch} = useQuery("tasks", async () => { return await taskplannerAPI.get('/tasks');});
+
     return (
         <Grid item md={8} px={4}>
             <Box mb={8} px={4}>
